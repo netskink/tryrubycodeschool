8637005e956c601f76daf867e0ab7bba7d560ee4
John F. Davis <davisjf@gmail.com>
2015-02-23 07:34:48 -0700

    Level 2 complete
    What happened
    
    [ticket: None]

---
+++ level2.rb
@@ -0,0 +1,37 @@
+
+
+# to_s converts values to strings
+a = 40.to_s.reverse
+print a
+print "\n"
+
+# to_i converts values to ints
+b = a.to_i
+print b
+print "\n"
+
+
+# an empty array
+x=[]
+x=[12,47,35]
+
+
+# to_a converts values to arrays
+c = x.to_a.reverse
+print c
+print "\n"
+
+# find max
+a = x.max
+print a
+print "\n"
+
+# sort
+a = x.sort
+print a
+print "\n"
+
+
+
+
+
